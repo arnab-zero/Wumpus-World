8ade711ae521567bc0428de945fe290244cd2830
arnab-zero <darnabhere@gmail.com>
2024-12-23 01:52:34 +0600

    Almost completed UserInterface.py

--- UserInterface.py
+++ UserInterface.py
@@ -18,13 +18,75 @@ MEDIUMSEAGREEN = (0, 250, 154)
 RADIUS = 0.45*SQUARE_LEN
 
 IMAGES = {}
+
+
+
 def loadImage():
     pics = ["agent_down1", "agent_left1", "agent_right1", "agent_up1", "breeze", "gold", "bg", "bg1", "pit", "stench", "wumpus" , "dead_wumpus", "arrow_overlay"]
     for pic in pics:
         IMAGES[pic] = pygame.transform.scale(pygame.image.load(f"./images/{pic}.png"), (SQUARE_LEN-1, SQUARE_LEN-1))
 
+
+
 def board_graphics_init():
-    return 
+    pygame.init()
+    board_width = B_R * SQUARE_LEN
+    board_height = (B_C+2) * SQUARE_LEN
+    screen = pygame.display.set_mode((board_width, board_height))
+    loadImage()
+    return screen
+
+
+
+def show_msg_up(txt, screen, color=WHITE):
+    pygame.draw.rect(screen, BLACK, (0, 0, B_C*SQUARE_LEN, 2*SQUARE_LEN))
+    font = pygame.font.SysFont('Verdana', 50)
+    text = font.render(txt, True, (color))
+    text_rect = text.get_rect(center=(B_C*SQUARE_LEN//2, SQUARE_LEN//2))
+    screen.blit(text, text_rect)
+    pygame.display.update()
+
+
+
+def show_msg_down(txt, screen, color=WHITE):
+    font = pygame.font.SysFont('Verdana', 40)
+    text = font.render(txt, True, (color))
+    text_rect = text.get_rect(center=(B_C*SQUARE_LEN//2, SQUARE_LEN//2+SQUARE_LEN))
+    screen.blit(text, text_rect)
+    pygame.display.update()
+
+
+
+def show_percept(tile, scream, screen):
+
+    dead_wump_img = IMAGES['dead_wumpus']
+    gold_img = IMAGES['gold']
+    breeze_img = IMAGES['breeze']
+    stench_img = IMAGES['stench']
+
+    per = 0
+    if tile.stench: per+=1
+    if tile.breeze: per+=1
+    if tile.gold:   per+=1
+
+    pos = -1*per//2
+    if tile.stench: 
+        pygame.draw.rect(screen, WHITE, (B_C*SQUARE_LEN//2+(35*pos), SQUARE_LEN//2+40, 30, 30))
+        screen.blit(stench_img, (B_C*SQUARE_LEN//2+(35*pos), SQUARE_LEN//2+40))
+        pos+=1
+    if tile.breeze: 
+        screen.blit(breeze_img, (B_C*SQUARE_LEN//2+(35*pos), SQUARE_LEN//2+40))
+        pos+=1
+    if tile.gold:   
+        screen.blit(gold_img, (B_C*SQUARE_LEN//2+(35*pos), SQUARE_LEN//2+40))
+        pos+=1
+    if scream:
+        screen.blit(dead_wump_img, (B_C*SQUARE_LEN//2+(35*pos), SQUARE_LEN//2+40))
+        pos+=1
+
+    pygame.display.update()
+
+
 
 def refreshBoardGraphics(board, dir, show_board, screen):
     # screen.fill((0, 0, 0))
@@ -32,7 +94,6 @@ def refreshBoardGraphics(board, dir, show_board, screen):
     for c in range(B_C):
         board[c], board[B_C-c-1] = board[B_C-c-1], board[c]
     
-
     bg_img = IMAGES['bg']
     wump_img = IMAGES['wumpus']
     player_right = IMAGES['agent_right1']
@@ -74,10 +135,11 @@ def refreshBoardGraphics(board, dir, show_board, screen):
             
             if not board[col][row].visited and not show_board:
                 screen.blit(alt_bg_img, pos)
-
-                
+      
     pygame.display.update()
     pass
+
+
 
 def choices(screen):
     
@@ -95,3 +157,24 @@ def choices(screen):
     screen.blit(text, text_rect)
 
     pygame.display.update()
+
+
+
+def mainMenu(screen):
+    choices(screen)
+    
+    while True:  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # For events that occur upon clicking the mouse (left click) 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+                print(B_C*SQUARE_LEN, B_R*SQUARE_LEN)
+                w_pad, h_pad = 100, 50
+                if (B_C*SQUARE_LEN//2)-w_pad <= event.pos[0] <= (B_C*SQUARE_LEN//2)+w_pad and (B_R*SQUARE_LEN//3)-h_pad <= event.pos[1] <= (B_R*SQUARE_LEN//3)+h_pad:
+                    return 1
+                elif (B_C*SQUARE_LEN//2)-w_pad <= event.pos[0] <= (B_C*SQUARE_LEN//2)+w_pad and (B_R*SQUARE_LEN//3+80)-h_pad <= event.pos[1] <= (B_R*SQUARE_LEN//3+80)+h_pad:
+                    return 2
+                elif (B_C*SQUARE_LEN//2)-w_pad <= event.pos[0] <= (B_C*SQUARE_LEN//2)+w_pad and (B_R*SQUARE_LEN//3+160)-h_pad <= event.pos[1] <= (B_R*SQUARE_LEN//3+160)+h_pad:
+                    return 3
